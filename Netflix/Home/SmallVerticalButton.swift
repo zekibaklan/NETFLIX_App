75b9e502d037425beea25498271bceac1968a460
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-12-01 17:06:38 +0300

    3.Commit ( MyListButton )

---
+++ Netflix/Home/SmallVerticalButton.swift
@@ -0,0 +1,18 @@
+//
+//  SmallVerticalButton.swift
+//  Netflix
+//
+//  Created by Zeki Baklan on 1.12.2023.
+//
+
+import SwiftUI
+
+struct SmallVerticalButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmallVerticalButton()
+}
---
+++ Netflix/Home/TopMoviePreview.swift
@@ -0,0 +1,18 @@
+//
+//  TopMoviePreview.swift
+//  Netflix
+//
+//  Created by Zeki Baklan on 1.12.2023.
+//
+
+import SwiftUI
+
+struct TopMoviePreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopMoviePreview()
+}
