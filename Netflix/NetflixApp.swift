df1a4edcf3b50ff3b317d07fdd42a4ecf040c2ef
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-12-01 12:55:06 +0300

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Zeki Baklan on 1.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
