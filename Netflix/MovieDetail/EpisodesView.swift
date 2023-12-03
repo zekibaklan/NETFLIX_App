9ec5e3cd208714e4a81e31cdedfdf77c0ac2ecca
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-12-03 20:35:32 +0300

    6.Commit ( EpisodeView )

---
+++ Netflix/MovieDetail/EpisodesView.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodesView.swift
+//  Netflix
+//
+//  Created by Zeki Baklan on 2.12.2023.
+//
+
+import SwiftUI
+
+struct EpisodesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodesView()
+}
