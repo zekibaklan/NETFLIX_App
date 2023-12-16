9e40a347d0c8e5845c01ad2aec1a243ec9843771
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-12-16 23:55:40 +0300

    PopularView Done

---
+++ Netflix/Search/PopularMovieView.swift
@@ -0,0 +1,18 @@
+//
+//  PopularMovieView.swift
+//  Netflix
+//
+//  Created by Zeki Baklan on 16.12.2023.
+//
+
+import SwiftUI
+
+struct PopularMovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopularMovieView()
+}
