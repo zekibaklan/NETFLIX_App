af28521155b1ba6389bbe911d2e8dcf5661839ea
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-12-18 00:02:27 +0300

    Search bar has been done

---
+++ Netflix/Search/SearchResultsGrid.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsGrid.swift
+//  Netflix
+//
+//  Created by Zeki Baklan on 17.12.2023.
+//
+
+import SwiftUI
+
+struct SearchResultsGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResultsGrid()
+}
