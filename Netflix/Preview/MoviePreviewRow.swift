9ff15bba4b26eded9ac73b69812bd99749abe1b5
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-12-30 03:01:38 +0300

    PagerView commit

---
+++ Netflix/Preview/MoviePreviewRow.swift
@@ -0,0 +1,18 @@
+//
+//  MoviePreviewRow.swift
+//  Netflix
+//
+//  Created by Zeki Baklan on 30.12.2023.
+//
+
+import SwiftUI
+
+struct MoviePreviewRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoviePreviewRow()
+}
---
+++ Netflix/Preview/PagerView.swift
@@ -0,0 +1,18 @@
+//
+//  PagerView.swift
+//  Netflix
+//
+//  Created by Zeki Baklan on 30.12.2023.
+//
+
+import SwiftUI
+
+struct PagerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PagerView()
+}
